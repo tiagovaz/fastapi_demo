e17a5034f933a23e44cb2ea407696afaebbaa9a0
Tiago Bortoletto Vaz <tiago@debian.org>
2025-06-01 09:59:29 -0400

    Split models.

--- 04-MVC-HTML-persistent/main.py
+++ 04-MVC-HTML-persistent/main.py
@@ -1,20 +1,15 @@
 from fastapi import FastAPI, HTTPException, Request, Form, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-from sqlmodel import SQLModel, Field, create_engine, Session, select
+from sqlmodel import SQLModel, create_engine, Session, select
 from typing import Optional
+from models.todo import TodoItem
 
 app = FastAPI(title="FastAPI To-Do App")
 
 # SQLite database setup
 DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(DATABASE_URL, echo=True)
-
-# SQLModel data model
-class TodoItem(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    title: str
-    completed: bool = False
 
 # Create tables on startup
 @app.on_event("startup")
@@ -37,9 +32,15 @@ async def read_root(request: Request, session: Session = Depends(get_session)):
 
 # Create todo
 @app.post("/todos", response_class=HTMLResponse)
+# Required ID:
 async def create_todo_form(request: Request, id: int = Form(...), title: str = Form(...)):
+# Optional ID (for auto-increment):
+#async def create_todo_form(request: Request, title: str = Form(...)):
     with Session(engine) as session:
+        # If we want an ID to be entered:
         todo = TodoItem(id=id, title=title)
+        # If we want auto-increment:
+        #todo = TodoItem(title=title)
         session.add(todo)
         session.commit()
         session.refresh(todo)
