dbb69dd70e55552e441689bae161704150c56b82
Tiago Bortoletto Vaz <tiago@debian.org>
2025-06-02 15:55:19 -0400

    Adding dotenv setup.

--- 04-MVC-HTML-persistent/database.py
+++ 04-MVC-HTML-persistent/database.py
@@ -1,11 +1,32 @@
 from sqlmodel import create_engine, Session, SQLModel
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./test.db"
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the database URL from the environment, otherwise use sqlite test.db
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+
+# Create the database engine
 engine = create_engine(DATABASE_URL, echo=True)
 
+# Create tables on startup
 def on_startup():
     SQLModel.metadata.create_all(engine)
 
+# Dependency to get a session
 def get_session():
     with Session(engine) as session:
         yield session
+
+###### .env file sample ###########
+## PostgreSQL database URL
+#DATABASE_URL_POSTGRESQL=postgresql+psycopg2://user:password@localhost/testdb
+#
+## MySQL database URL
+#DATABASE_URL_MYSQL=mysql+pymysql://user:password@localhost/testdb
+#
+## SQLite database URL
+#DATABASE_URL_SQLITE=sqlite:///./test.db
+
